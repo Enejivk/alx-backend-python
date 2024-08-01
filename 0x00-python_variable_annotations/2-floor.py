64e59d216334ba05856ffedc3a942c1087d0db76
enejivk <enejivic@gmail.com>
2024-08-01 12:34:32 +0100

    making  changes to the code

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,7 @@
-import math
+#!/bin/usr/bin/env python3
 """round down float"""
 
 
 def floor(n:float) ->int:
     """It takes in float and round it down"""
-    return math.floor(n)
+    return int(n)
