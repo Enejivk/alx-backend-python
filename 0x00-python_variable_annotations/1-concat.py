7d07b574ebc72925191440f76e724c8d95c67ede
enejivk <enejivic@gmail.com>
2024-08-01 11:25:52 +0100

    a type-annotated function floor which takes a float n as argument and returns the floor of the float

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def concat(str1:str, str2:str) -> str:
+  """It takes a two string and return
+      concatenated string
+  Args:
+      str1 (str): the first string
+      str2 (str): the second string
+  Returns:
+      str: this return the concatenated string
+  """
+  return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+import math
+def floor(n:float) ->int:
+  """It takes in float and round it down
+      to the nearest integer
+  Args:
+      n (float):This is the number
+  Returns:
+      int: round down of the float
+  """
+  return math.floor(n)
