077c7b2a8135451a4f61034642d7ccc2c0f5f20b
enejivk <enejivic@gmail.com>
2024-08-01 12:17:07 +0100

    adding comment to the module

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+"""add tow numbers"""
+
+
 def add(a: float, b: float) -> float:
     """
     Add two floating-point numbers and return the result.
-
     Args:
         a (float): The first number.
         b (float): The second number.
-
     Returns:
         float: The sum of the two numbers.
     """
