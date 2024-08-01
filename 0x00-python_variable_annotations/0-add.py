5f1620d264632a0ca5366f9b1eb881c89f19c1df
enejivk <enejivic@gmail.com>
2024-08-01 11:01:33 +0100

    a type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float.

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """
+    Add two floating-point numbers and return the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of the two numbers.
+    """
+    return a + b
