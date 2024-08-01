6eb79220d91807527821e35eb64933b01350b020
enejivk <enejivic@gmail.com>
2024-08-01 12:43:02 +0100

    fixing indentation problem

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
+"""return a string"""
 
-def to_str(n:float) ->str:
-  """It takes a number and convert it to string
-  Args:
-      n (float): number to be string
-  Returns:
-      str: it return a converted string
-  """
-  return str(n)
+
+def to_str(n: float) -> str:
+    """It takes a number and convert it to string"""
+    return str(n)
