31a347e1fa8591155393cd80459ff6cc70a6676c
enejivk <enejivic@gmail.com>
2024-08-01 12:46:51 +0100

    adding commment to the code

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""defining variables"""
+"""Defines annotated variables"""
 
 
-a:int = 1
-pi:float = 3.14
-i_understand_annotations:bool = True
-school:str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
