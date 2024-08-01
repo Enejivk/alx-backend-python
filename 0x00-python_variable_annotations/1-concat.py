849bdcc7d65c1972a00421376eb379dcca37f585
enejivk <enejivic@gmail.com>
2024-08-01 12:09:16 +0100

    fixing indentation problem

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """ This return concatenated string"""
-def concat(str1:str, str2:str) -> str:
-  """It takes a two string and return
+
+
+def concat(str1: str, str2: str) -> str:
+    """It takes a two string and return
       concatenated string
-  Args:
-      str1 (str): the first string
-      str2 (str): the second string
-  Returns:
-      str: this return the concatenated string
-  """
-  return str1 + str2
+      Args:
+        str1 (str): the first string
+        str2 (str): the second string
+      Returns:
+        str: this return the concatenated string
+        """
+    return str1 + str2
