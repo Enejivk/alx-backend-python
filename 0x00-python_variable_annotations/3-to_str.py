29c6b80493a597b91e1dc9847c69527a82165eaa
enejivk <enejivic@gmail.com>
2024-08-01 11:46:35 +0100

    Define and annotate the following variables with the specified values:
    a, an integer with a value of 1
    pi, a float with a value of 3.14
    i_understand_annotations, a boolean with a value of True
    school, a string with a value of “Holberton

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def to_str(n:float) ->str:
+  """It takes a number and convert it to string
+  Args:
+      n (float): number to be string
+  Returns:
+      str: it return a converted string
+  """
+  return str(n)
