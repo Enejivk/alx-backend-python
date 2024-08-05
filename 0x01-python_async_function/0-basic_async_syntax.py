827377ef7a936c07fe94b3d1d8b960197fc38d91
enejivk <enejivic@gmail.com>
2024-08-05 20:50:26 +0100

    making some correction to the code

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Modules that return a random number"""
 
-import random
 import asyncio
+import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """Function that returns a random number after a delay"""
     wait_time = random.uniform(0, max_delay)
     await asyncio.sleep(wait_time)
