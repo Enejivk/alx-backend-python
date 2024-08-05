78be268979d9de6f696bf8468b7a0d884bcae429
enejivk <enejivic@gmail.com>
2024-08-05 22:14:37 +0100

    sorted list

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,16 +2,19 @@
 """This return sorted list"""
 import asyncio
 from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[int]:
     """This function return sorted list"""
-    task = [wait_random(max_delay) for spawn_time in range(n)]
-    each_task = await asyncio.gather(*task)
 
-    for i in range(len(each_task)):
-        for y in range(1, len(each_task)):
-            if each_task[y-1] > each_task[y]:
-                each_task[y-1], each_task[y] = each_task[y], each_task[y-1]
-    return each_task
+    task = [wait_random(max_delay) for spawn_time in range(n)]
+    res = await asyncio.gather(*task)
+
+    for i in range(len(res)):
+        for y in range(1, len(res)):
+            if res[y-1] > res[y]:
+                res[y-1], res[y] = res[y], res[y-1]
+    return res
